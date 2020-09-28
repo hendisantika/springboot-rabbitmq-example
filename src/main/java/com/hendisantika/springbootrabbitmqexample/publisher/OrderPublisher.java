f8af1ec79dc7a7dc359d17d1f01039e1351409b0
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-29 05:33:52 +0700

    #7 - Add Order Publisher

---
+++ src/main/java/com/hendisantika/springbootrabbitmqexample/publisher/OrderPublisher.java
@@ -0,0 +1,41 @@
+package com.hendisantika.springbootrabbitmqexample.publisher;
+
+import com.hendisantika.springbootrabbitmqexample.config.MessagingConfig;
+import com.hendisantika.springbootrabbitmqexample.dto.Order;
+import com.hendisantika.springbootrabbitmqexample.dto.OrderStatus;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-rabbitmq-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 29/09/20
+ * Time: 05.33
+ */
+@RestController
+@RequestMapping("/order")
+public class OrderPublisher {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @PostMapping("/{restaurantName}")
+    public String bookOrder(@RequestBody Order order, @PathVariable String restaurantName) {
+        order.setOrderId(UUID.randomUUID().toString());
+        //restaurantservice
+        //payment service
+        OrderStatus orderStatus = new OrderStatus(order, "PROCESS", "order placed succesfully in " + restaurantName);
+        template.convertAndSend(MessagingConfig.EXCHANGE, MessagingConfig.ROUTING_KEY, orderStatus);
+        return "Success !!";
+    }
+}
