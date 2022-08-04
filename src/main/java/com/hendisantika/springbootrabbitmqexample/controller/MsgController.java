3725738a8e62d8f4bf9e0bc4fd857e2f81968d76
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-04 15:14:04 +0700

    #59 | Hendi Santika | Add sendMessage API in MsgController :pencil2: :green_book:

--- src/main/java/com/hendisantika/springbootrabbitmqexample/controller/MsgController.java
+++ src/main/java/com/hendisantika/springbootrabbitmqexample/controller/MsgController.java
@@ -1,9 +1,17 @@
 package com.hendisantika.springbootrabbitmqexample.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.springbootrabbitmqexample.dto.Corporation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,10 +23,25 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 15:10
  * To change this template use File | Settings | File Templates.
  */
+@Slf4j
 @RestController
 @EnableBinding(Source.class)
 @RequiredArgsConstructor
 public class MsgController {
 
     private final Source source;
+
+    @PostMapping(value = "/get-details")
+    public String sendMessage(@RequestBody String payload) throws IOException {
+        ObjectMapper ob = new ObjectMapper();
+        Corporation corporation = null;
+        try {
+            corporation = ob.readValue(payload, Corporation.class);
+        } catch (Exception ex) {
+            log.info(String.valueOf(ex));
+        }
+        source.output().send(MessageBuilder.withPayload(corporation).setHeader("myheader", "myheaderValue").build());
+        log.info("success");
+        return "Message sent to RabbitMQ";
+    }
 }
