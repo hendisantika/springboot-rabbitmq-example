888a35478386c91e04e1e13ddc19cf9611e78f41
Hendi Santika <hendisantika@yahoo.co.id>
2022-08-04 17:56:56 +0700

    #64 | Hendi Santika | Add amqpTemplate in RabbitMQConfig :bowtie: :phone:

--- src/main/java/com/hendisantika/springbootrabbitmqexample/config/RabbitMQConfig.java
+++ src/main/java/com/hendisantika/springbootrabbitmqexample/config/RabbitMQConfig.java
@@ -1,6 +1,14 @@
 package com.hendisantika.springbootrabbitmqexample.config;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,4 +36,25 @@ public class RabbitMQConfig {
         return new Queue(QUEUE, true);
     }
 
+    @Bean
+    DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
 }
